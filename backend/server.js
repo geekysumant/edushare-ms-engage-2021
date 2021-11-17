4b9e2c548bec851718a1edaa8f79ed78d3f8539d
Sumant <sumant@Sumants-MacBook-Air.local>
2021-11-17 22:31:05 +0530

    fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 
 //database connection
 const db = require("./config/mongoose");
