65e38d0b633a43eace5a8e608477f4d77bcb8397
Sumant <sumant@Sumants-MacBook-Air.local>
2021-11-17 21:58:27 +0530

    testing deployment

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,14 @@ app.use(express.json());
 
 app.use("/", require("./routes"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+}
+
 app.listen(8080, (err) => {
   if (err) console.log(err);
   console.log("Server up & running!");
